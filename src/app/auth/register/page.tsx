8d2c9066df4eecbea21eb47e1ceb03dbd2ce19ec
Henok <henokaragaw17@gmail.com>
2025-05-24 02:51:07 -0900

    betterauth and auth page setup

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='px-8 py-16 container mx-auto max-w-screen-lg y-8'>
+        <div className='space-y-8'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Register
